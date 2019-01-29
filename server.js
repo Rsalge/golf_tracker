ad947c9d46b21287fe3ae22d3c5c33ae8d441c37
Ross Salge <rossnsalge@gmail.com>
2019-01-28 18:30:53 -0600

    Setup passport local startegy for User login/logout

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 const db = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${
   process.env.DB_HOST
 }`;
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
 
 // connect to mongoDB
 mongoose
@@ -18,10 +20,16 @@ mongoose
     console.log(err);
     console.log("\x1b[31m\x1b[1m MongoDB Not Connected");
   });
+// print mongoose queries
+mongoose.set("debug", true);
 
-// console.log that your server is up and running
+// print that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+//Models and routes
+require("./models/Users");
+// passort config must be below all models
+require("./config/passport");
 // create a GET route
 app.get("/express_backend", (req, res) => {
   res.send({ express: "YOUR EXPRESS TEST IS CONNECTED TO REACT" });
