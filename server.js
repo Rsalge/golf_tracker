b912a1a792f6f9eb01f753644f7aabfcc0b4a98e
Ross Salge <rossnsalge@gmail.com>
2019-01-29 18:05:04 -0600

    Implemented Passport Local Strategy with JWT, basic user creation in place

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
 const db = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${
@@ -22,6 +23,12 @@ mongoose
   });
 // print mongoose queries
 mongoose.set("debug", true);
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 
 // print that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -30,6 +37,8 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 require("./models/Users");
 // passort config must be below all models
 require("./config/passport");
+app.use(require("./routes"));
+
 // create a GET route
 app.get("/express_backend", (req, res) => {
   res.send({ express: "YOUR EXPRESS TEST IS CONNECTED TO REACT" });
